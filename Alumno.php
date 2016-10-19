fc075a16c06031561e50a9e41e706903cad8326b
Your Name <you@example.com>
2016-10-19 12:40:25 +0200

    second commit

---
+++ Alumno.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ejercicio if</title>
+  </head>
+  <body>
+    <?php
+    Class Alumno
+{
+    //Declaración de los aributos
+public $Nombre ="";
+public $Edad ="";
+public $Curso ="";
+public $Ciclo ="";
+
+//Getters y Setters
+public function setNombre($nuevoNombre){
+$this->Nombre=$nuevoNombre;
+}
+
+public function getNombre(){
+return $this->Nombre;
+}
+
+//Declaracio de condiciones
+public function setEdad($nuevoEdad){
+if ($nuevoEdad<18){
+    $this->Edad=18;
+  }else{
+    $this->Edad=$nuevoEdad;
+  }
+}
+public function getEdad(){
+return $this->Edad;
+}
+
+    public function setCurso ($nuevoCurso){
+      if ($nuevoCurso=1){
+        $this->Curso=$nuevoCurso;
+      }elseif ($nuevoCurso=2){
+        $this->Curso=$nuevoCurso;
+      }else{
+        $this->curso=1;
+      }
+    }
+
+    public function getCurso (){
+      return $this->Curso;
+}
+  //Ciclo
+    public function setCiclo($nuevoCiclo){
+      $this->Ciclo=$nuevoCiclo;
+  }
+
+    public function getCiclo(){
+      return $this->Ciclo;
+  }
+}
+?>
